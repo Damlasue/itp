fa535c34130033b12b2be2af935d04f2992e60f0
Damlasue <124000969+Damlasue@users.noreply.github.com>
2023-03-08 17:25:18 -0500

    yess

--- damlasuKiyak_print_graph(n):.py
+++ damlasuKiyak_print_graph(n):.py
@@ -1,25 +1,3 @@
-**Think Phyton chapter 10
-# 1 thing I knew: 
-I knew that one way to make a list is to use square brackets ([ and ])!
-
-
-# 1 thing I didn't know:
-I did not know that you could use delete as a function, it simply makes things easier. (When you pass a list to a function, the function gets a reference to the list. If the function modifies the list, the caller sees the change. For example, delete_head removes the first element from a list:)
-Example listed from article:
->>> letters = ['a', 'b', 'c']
->>> delete_head(letters)
->>> letters
-['b', 'c']
-
-I also didnt know that lists were mutable! thats cool!
-
-
-
-
-
-
-
-
 for i in range (64):
     print ("*", end="")
 print ()
